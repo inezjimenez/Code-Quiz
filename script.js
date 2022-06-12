77f4d5aebad2bf20033bbf8fcbf3c0d1fbedcda0
Inez Jimenez <ijim9182@yahoo.com>
2022-06-12 12:18:05 -0500

    Stylesheet updated and beginning script

---
+++ script.js
@@ -0,0 +1,79 @@
+// creating quiz class
+class Quiz {
+    constructor(questions) {
+        this.score=0;
+        this.questions=questions;
+        this.questionIndex=0;
+    }
+
+    getQuestionIndex() {
+        return this.questions{this.questionIndex};
+    }
+
+    quess(answer) {
+        if(this.getQuestionIndex().isCorrectAnswer(answer))
+        this.score++;
+    }
+    this.questionIndex++;
+
+    isEnded() {
+        return this.questionIndex === this.questions.legnth;
+    }
+
+}
+
+//creating question class
+class Question {
+    constructor(text,choices,answer) {
+        this.text=text;
+        this.choices=choices;
+        this.answer=answer;
+    }
+
+    isCorrectAnswer(choice) {
+        return this.answer===choice;
+    }
+}
+
+//displaying questions
+function displayQuestion() {
+    if (quiz.isEnded()) {
+        showScores();
+    }else{
+        //show question
+        let questionElement=document.getElementById("question");
+        questionElement=quiz.getQuestionIndex().text;
+
+        //show options
+        let choices=quiz.getQuestionIndex().choices;
+        for(let i=0;i<choices.legnth;i++) {
+            let choiceElement=document.getElementById("choice"+i);
+            choiceElement.innerHTML=choices[i]
+            guess("btn"+i,choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+//creating guess function
+function guess(id,guess){
+    let button=document.getElementById(id);
+    button.oneclick=function(){
+        quiz.guess(guess);
+        displayQuestion();
+    }
+}
+
+//show quiz progress
+function showProgress() {
+    let currentQuestionNumber=quiz.questionIndex + 1;
+    let progressElement= document.getElementById("progress")
+    pregressElement.innerHTML=
+    'Question ${currentQuestionNumber} of ${quiz.question.length}';
+}
+
+//show score
+function showScores() {
+    let quizEndHTML=
+}
